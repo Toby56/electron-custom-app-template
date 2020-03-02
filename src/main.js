6498bfe5d49a3b0c10e297b5a6caf6c0363bdbe3
Toby56 <Toby56@pm.me>
2020-03-02 16:44:38 +1100

    Many improvements to JS, CSS and HTML structure

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-const electron = require("electron");
+const { app, BrowserWindow, shell } = require("electron");
 
-electron.app.whenReady().then(() => {
+app.whenReady().then(() => {
   const { width, height, x, y } = (windowStateKeeper = require("electron-window-state")({
     defaultWidth: 800,
     defaultHeight: 600
   }));
 
-  win = new electron.BrowserWindow({
+  win = new BrowserWindow({
     x: x,
     y: y,
     width: width,
@@ -20,77 +20,106 @@ electron.app.whenReady().then(() => {
 
   windowStateKeeper.manage(win);
 
+  win.webContents.on("will-navigate", (event, url) => {
+    function domain(url) {
+      var match = url.match(/(?:\/\/([^\/?#]*))/i);
+      return !match ? match[1] : "";
+    }
+    if (domain(win.webContents.getURL()) != domain(url)) {
+      event.preventDefault();
+      shell.openExternal(url);
+    }
+  });
+
+  win.webContents.on("new-window", (event, url) => {
+    event.preventDefault();
+    shell.openExternal(url);
+  });
+
   win.webContents.on("dom-ready", () => {
-    win.webContents.executeJavaScript(
-      "(" +
-        (() => {
-          const remote = require("electron").remote;
+    win.webContents
+      .executeJavaScript(
+        "(" +
+          (() => {
+            try {
+              const remote = require("electron").remote;
 
-          var win = remote.getCurrentWindow();
+              var win = remote.getCurrentWindow();
 
-          document.getElementById("window-title").innerHTML = document.title;
+              win.on("page-title-updated", (event, title) => {
+                document.getElementById("titlebar-text").innerHTML = title;
+              });
 
-          new MutationObserver(function(mutations) {
-            document.getElementById("window-title").innerHTML = mutations[0].target.data;
-          }).observe(document.querySelector("title"), { characterData: true, subtree: true });
+              document.getElementById("titlebar-text").innerHTML = document.title;
 
-          document.getElementById("window-controls-minimize").addEventListener("click", event => {
-            win.minimize();
-          });
+              document.getElementById("titlebar-buttons-minimize").addEventListener("click", event => {
+                win.minimize();
+              });
 
-          document.getElementById("window-controls-maximize").addEventListener("click", event => {
-            win.maximize();
-          });
+              document.getElementById("titlebar-buttons-maximize").addEventListener("click", event => {
+                win.maximize();
+              });
 
-          document.getElementById("window-controls-restore").addEventListener("click", event => {
-            if (win.isFullScreen()) {
-              win.setFullScreen(false);
-            } else {
-              win.unmaximize();
+              document.getElementById("titlebar-buttons-restore").addEventListener("click", event => {
+                if (win.isFullScreen()) {
+                  win.setFullScreen(false);
+                } else {
+                  win.unmaximize();
+                }
+              });
+
+              document.getElementById("titlebar-buttons-close").addEventListener("click", event => {
+                win.close();
+              });
+
+              toggleMaximized();
+              win.on("maximize", toggleMaximized);
+              win.on("unmaximize", toggleMaximized);
+
+              function toggleMaximized() {
+                if (win.isMaximized()) {
+                  document.body.classList.add("maximized");
+                } else {
+                  document.body.classList.remove("maximized");
+                }
+              }
+
+              toggleBlurred();
+              win.on("blur", toggleBlurred);
+              win.on("focus", toggleBlurred);
+
+              function toggleBlurred() {
+                if (win.isFocused()) {
+                  document.body.classList.remove("blurred");
+                } else {
+                  document.body.classList.add("blurred");
+                }
+              }
+
+              toggleFullScreen();
+              win.on("enter-full-screen", toggleFullScreen);
+              win.on("leave-full-screen", toggleFullScreen);
+
+              function toggleFullScreen() {
+                if (win.isFullScreen()) {
+                  document.body.classList.add("full-screen");
+                } else {
+                  document.body.classList.remove("full-screen");
+                }
+              }
+            } catch (error) {
+              return error;
             }
-          });
-
-          document.getElementById("window-controls-close").addEventListener("click", event => {
-            win.close();
-          });
-
-          toggleMaximized();
-          win.on("maximize", toggleMaximized);
-          win.on("unmaximize", toggleMaximized);
-
-          function toggleMaximized() {
-            if (win.isMaximized()) {
-              document.body.classList.add("maximized");
-            } else {
-              document.body.classList.remove("maximized");
-            }
+          }).toString() +
+          ")()"
+      )
+      .then(result => {
+        if (result) {
+          if (result instanceof Error) {
+            console.error(result);
+            app.quit();
           }
-
-          toggleBlurred();
-          win.on("blur", toggleBlurred);
-          win.on("focus", toggleBlurred);
-
-          function toggleBlurred() {
-            if (win.isFocused()) {
-              document.body.classList.remove("blurred");
-            } else {
-              document.body.classList.add("blurred");
-            }
-          }
-
-          toggleFullScreen();
-          win.on("enter-full-screen", toggleFullScreen);
-          win.on("leave-full-screen", toggleFullScreen);
-
-          function toggleFullScreen() {
-            if (win.isFullScreen()) {
-              document.body.classList.add("full-screen");
-            } else {
-              document.body.classList.remove("full-screen");
-            }
-          }
-        }).toString() +
-        ")()"
-    );
+        }
+      });
   });
 });
